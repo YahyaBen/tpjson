c05ef3a6a3a8f0a5e89cfcf6e2bae835c2598f32
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2021-02-18 10:48:06 +0100

    StopWatch 10 to 1000

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { useState, useEffect, useRef } from 'react'
 import Button from './components/Button.js'
 import Users from './components/Users.js'
 import Form from './components/Form.js';
-import Parent from './components/Parent.js';
+import Timers from './components/Timers.js';
+
 
 function App() {
-  const count = useRef (2)
+  const count = useRef (1)
   const [users, setUsers] = useState(
     [
         {
@@ -42,29 +43,93 @@ function App() {
       },
     ]
 )
-
-// Use effects
-useEffect(()=>{
-alert("Test use effect number :")
-count.current = count.current +1
-},[users])
-
 // Recuperation de 'ID de l'utilisateur
 var u ;
 const selectID = (id)=>{
   u = id;
   console.log({"u": u});
 }
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//////////////////////////Stop Watch //////////////////////////
+///////////////////////////////////////////////////////////////
+const [timer, setTimer] = useState(0)
+const [isActive, setIsActive] = useState(false)
+const [isPaused, setIsPaused] = useState(false)
+const [isSelected,SetIsSelected] = useState(false)
+const countRef = useRef(0)
+
+const Start = () => {
+  // start button logic here
+  setIsActive(true)
+  setIsPaused(true)
+  countRef.current = setInterval(() => {
+    setTimer((timer) => timer + 1)
+  }, 10)
+}
+
+const Pause = () => {
+  // Pause button logic here
+  clearInterval(countRef.current)
+  setIsPaused(false)
+}
+
+const Resume = () => {
+  // Resume button logic here
+  setIsPaused(true)
+  countRef.current = setInterval(() => {
+    setTimer((timer) => timer + 1)
+  }, 10)
+}
+
+const Reset = () => {
+  // Reset button logic here
+  clearInterval(countRef.current)
+  setIsActive(false)
+  setIsPaused(false)
+  setTimer(0)
+}
+
+///////////////////////// Formatting Time ////////////////////
+//////////////////////////////////////////////////////////////
+const formatTime = () => {
+  const getSeconds = `0${(timer % 60)}`.slice(-2)
+  const minutes = `${Math.floor(timer / 60)}`
+  const getMinutes = `0${minutes % 60}`.slice(-2)
+  const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+  return getHours+ `:` +getMinutes+ `:`+getSeconds 
+
+}
+
+
+  // return  User +`  est en pause depuis : ${getHours} : ${getMinutes} : ${getSeconds}`
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+// Use effects
+useEffect(()=>{
+// alert("Test use effect number :"+count.current)
+// count.current = count.current +1
+},[users])
 
 // Demander une pause
 var d=1;
 const Demander = () => {
-  const id =Math.max.apply(Math,users.map(function(A){return A.id}))+1
-  const pseudo = "Mohammed"
-  const password ="BBB"
-  const team =[ ]
-  const newUser = {id,pseudo,password,team}
-  setUsers([...users,newUser])
+
+  var x = users.find(e=> e.id == u);
+  var index = users.indexOf(x);
+  alert(users[index].pseudo+" Veux une pause")
+  console.log(users[u])
+  // const id =Math.max.apply(Math,users.map(function(A){return A.id}))+1
+  // const pseudo = "Mohammed"
+  // const password ="BBB"
+  // const team =[ ]
+  // const newUser = {id,pseudo,password,team}
+  // setUsers([...users,newUser])
 }
 
  //Modifier Position / Prolanger une Pause
@@ -97,15 +162,15 @@ const addUser = (user) => {
   setUsers([...users, newUser])
 }
 
-// Users Up
+// Users Up (pas terminer)
 const goUP = () => {
   var A =users
   A[u] = A.splice(1,1,A[u])[0]
-alert(A)
+  alert(A)
 
 }
 
-// Users down
+// Users down (pas terminer)
 const goDown = (user) => {
   const id = Math.max.apply(Math,users.map(function(A){return A.id}))+1
   const newUser={id,...user}
@@ -113,18 +178,28 @@ const goDown = (user) => {
 }
   return (
     <div className="flex-container">
-      <p>
-        {count.current}
-      </p>
     <div className="Form"><Form onAdd={addUser}/></div>
     <div className="List"><Users users={users} selectid={selectID} /></div>
     <div className="Button">
       <Button color='Green' text='Demander' onClick={Demander} />
       <Button color='Orange' text='Prolonger' onClick={Prolonger} />
       <Button color='Red' text='Supprimer'  onClick={Supprimer}/>
-      {/* <Button color='grey' text='Up'  onClick={goUP}/> // c'est fini */}
-      {/* <Button color='grey' text='SupprDownr'  onClick={goDown}/> */}
-      <Parent />
+    </div>
+    
+    <div>
+        <Timers time={formatTime()}/>
+    </div>
+
+    <div className="Button">
+    {
+            !isActive && !isPaused ?
+            <Button color='Blue' text='Start' onClick={Start} /> 
+            :
+            (isPaused ?       <Button color='Blue' text='Pause' onClick={Pause} /> : 
+            <Button color='Blue' text='Resumer'  onClick={Resume}/>
+            )
+          }
+          <Button color='Blue' text='Reset'  onClick={Reset}/>  
     </div>
     </div>
   );
---
+++ src/components/Timers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Timers = ({time}) => {
+    return (
+        <div>
+            <p> {time}</p>
+        </div>
+    )
+}
+
+export default Timers
